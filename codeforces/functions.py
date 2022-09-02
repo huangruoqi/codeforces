3564543b1597f6ecd0f50d31fb9f215817010814
Ruoqi Huang <huangruoqi374@gmail.com>
2022-09-01 17:28:38 -0700

    reset contest

--- codeforces/functions.py
+++ codeforces/functions.py
@@ -73,6 +73,7 @@ def f_push():
         return
     if os.system(f"git push origin C-{get_contest_id()}")==0:
         os.system("git checkout master")
+        set_contest(None, None)
     else:
         print("Undoing lasest commit...")
         os.system("git reset --soft HEAD~1")
